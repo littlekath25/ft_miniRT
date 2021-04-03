77e04f68c81966a22704f56febbd7bbc83012659
Katherine Fu <katherine-fu@outlook.com>
2021-04-03 18:45:55 +0200

    commit

--- src/intersect/ft_plane.c
+++ src/intersect/ft_plane.c
@@ -6,7 +6,7 @@
 /*   By: katherine <katherine@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/03 12:23:53 by katherine     #+#    #+#                 */
-/*   Updated: 2021/04/03 18:40:23 by katherine     ########   odam.nl         */
+/*   Updated: 2021/04/03 18:45:50 by katherine     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void		ft_intersect_plane(t_ray *ray, t_impact *impact, t_camera *camera, t_plane
 	double		t_near;
 	t_vector	ray_object;
 
-	denom = -ft_dot_product(object->ori, ray->dir);
+	denom = ft_dot_product(object->ori, ray->dir);
 	if (fabs(denom) > 0) {
 		ray_object = ft_subtract(object->pos, ray->pos);
-		t_near = -ft_dot_product(ray_object, object->ori) / denom;
+		t_near = ft_dot_product(ray_object, object->ori) / denom;
 		if (t_near > 1e-4 && t_near < impact->near)
 		{
 			impact->near = t_near;
